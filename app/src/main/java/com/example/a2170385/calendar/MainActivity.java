01cfb6ce43af4e40aa2a219701fd8d203c62dc60
unknown <2170385@maetel.ecc.ac.jp>
2018-06-05 19:22:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a2170385/calendar/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.a2170385.calendar;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.*;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView titleText;
+    private TextView dateText;
+    private Button prevButton, nextButton;
+    private DateManager mDateManager;
+    private CalendarAdapter mCalendarAdapter;
+    private GridView calendarGridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        titleText = (TextView) findViewById(R.id.titleText);
+        prevButton = (Button) findViewById(R.id.prevButton);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.prevMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+
+        nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.nextMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+
+        calendarGridView = (GridView) findViewById(R.id.calendarGridView);
+        mCalendarAdapter = new CalendarAdapter(this);
+        calendarGridView.setAdapter(mCalendarAdapter);
+        titleText.setText(mCalendarAdapter.getTitle());
+        calendarGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCalendarAdapter.getItem(position);
+
+            }
+        });
+    }
+
+}
